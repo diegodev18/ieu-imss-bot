997fa4e73458a72600df3a86fbfabcb190b82357
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-25 09:35:20 -0600

    Remove welcome and help message replies from bot commands

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,7 @@
 import { bot } from "@/lib/telegram-bot";
 
-bot.start((ctx) =>
-  ctx.reply(
-    "Bienvenido! Usa /auth y la contraseña que se te asigno para obtener permisos de administrador de personal del IMS",
-  ),
-);
+bot.start((ctx) => {});
 
-bot.help((ctx) =>
-  ctx.reply(`\
-La funcion de este bot es ayudarte a administrar el personal del IMS de una manera eficiente y productiva.
-
-Si no estas autentificado, no podras usar los comandos... Usa /auth <usuario> <contraseña> para autentificarte.
-
-Puedes usar los comandos:
-- /new: Registra un nuevo miembro del personal`),
-);
+bot.help((ctx) => {});
 
 bot.launch();
