48a7846908bea2afa42a078c6d843c01918eb78e
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-25 09:38:36 -0600

    Refactor command types to improve type safety and consistency in command definitions

--- src/utils/commands/index.ts
+++ src/utils/commands/index.ts
@@ -7,14 +7,18 @@ interface Command {
   action: (ctx: any) => void;
 }
 
-export const commands: Record<string, Command> = {
+type Commands = "auth";
+
+type SeoCommands = "start" | "help";
+
+export const commands: Record<Commands, Command> = {
   auth: {
     description: "Autentificate as admin",
     action: authCommand,
   },
 };
 
-export const seoCommands: Record<string, Command> = {
+export const seoCommands: Record<SeoCommands, Command> = {
   start: {
     description: "Start the bot",
     action: startCommand,
