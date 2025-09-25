f938fe72c6453aefe2d286b58b9015014c8a4ddb
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-25 09:41:42 -0600

    Implement command actions for start and help bot commands

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import { bot } from "@/lib/telegram-bot";
+import { commands, seoCommands } from "@/utils/commands";
 
-bot.start((ctx) => {});
+bot.start((ctx) => {
+  seoCommands.start.action(ctx);
+});
 
-bot.help((ctx) => {});
+bot.help((ctx) => {
+  seoCommands.help.action(ctx);
+});
+
+Object.entries(commands).forEach(([command, { action }]) => {
+  bot.command(command, (ctx) => action(ctx));
+});
 
 bot.launch();
