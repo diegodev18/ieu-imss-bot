f847ea3370c39858dec9f3fbd763cfb5773e0987
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-25 09:29:17 -0600

    Fix typo in bot help message and clarify authentication instructions

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ bot.start((ctx) =>
 
 bot.help((ctx) =>
   ctx.reply(`\
-La funcion de este bot es ayudarte a administrar el perrsonal del IMS de una manera eficiente y productiva.
+La funcion de este bot es ayudarte a administrar el personal del IMS de una manera eficiente y productiva.
 
-Si no estas autentificado, no podras usar los comandos...
+Si no estas autentificado, no podras usar los comandos... Usa /auth <usuario> <contraseña> para autentificarte.
 
 Puedes usar los comandos:
 - /new: Registra un nuevo miembro del personal`),
