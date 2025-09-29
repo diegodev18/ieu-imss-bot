7247b2ae2349179e1b6486718a97f5f76233c51d
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-29 08:54:56 -0600

    feat: implement async authentication logic in authCommand

--- src/utils/commands/auth.ts
+++ src/utils/commands/auth.ts
@@ -1,10 +1,24 @@
 import { Context } from "telegraf";
+import { prisma } from "@/lib/prisma";
 
-export const authCommand = (ctx: Context) => {
+export const authCommand = async (ctx: Context) => {
   let message: string | undefined;
   if (ctx.message && "text" in ctx.message) {
     message = (ctx.message as { text: string }).text;
   }
 
-  ctx.reply(`Received message: ${message}`);
+  const tokens = message?.split(" ");
+  const user = tokens?.[1];
+  const pass = tokens?.[2];
+
+  if (!user || !pass) {
+    await ctx.reply("Usage: /auth <username> <password>");
+    return;
+  }
+
+  const isAuth = await prisma.admin_user_tokens.findFirst({
+    where: { user: parseInt(user), password: pass },
+  });
+  if (isAuth) {
+  }
 };
