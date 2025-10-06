886bcc1fc43014c3fd0be5ce9d22fa0e027e174b
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-06 00:39:34 -0600

    fix: restructure Session type definition and update ContextWithSession to use it

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,18 @@
-export type ContextWithSession = Context & {
-  session?: {
+export type Session = {
+  id: number;
+  chat_id: number;
+  admin_id: number;
+  user_metadata: Object;
+  company: {
     id: number;
-    chat_id: number;
-    admin_user_tokens_id: number;
-    user_metadata: string;
+    name: string;
+    rfc: string;
+    status: "active" | "inactive";
     created_at: Date;
   };
+  created_at: Date;
+};
+
+export type ContextWithSession = Context & {
+  session?: Session;
 };
