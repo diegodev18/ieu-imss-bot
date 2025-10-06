57e3e94e9df100cab238d64488a404f861f3dd28
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-10-06 15:40:32 -0600

    feat: add search tool for querying employee database

---
+++ src/utils/tools/search.ts
@@ -0,0 +1,62 @@
+import { prisma } from "@/lib/prisma";
+import { Type } from "@google/genai";
+
+const searchToolParams = {
+  type: Type.OBJECT,
+  properties: {
+    name: {
+      type: Type.STRING,
+      description: "The name of employee to search for in the database",
+    },
+    curp: {
+      type: Type.STRING,
+      description: "The CURP of employee to search for in the database",
+    },
+    limit: {
+      type: Type.INTEGER,
+      description: "Number of results to return",
+      default: 5,
+    },
+  },
+  required: ["query"],
+} as const;
+
+export const searchToolDeclaration = {
+  name: "search_tool",
+  description: "Use this tool to search for items in the database.",
+  parameters: searchToolParams,
+};
+
+export const searchTool = async (name: string, curp: string, limit = 5) => {
+  try {
+    const results = await prisma.employees.findMany({
+      where: {
+        AND: [
+          name
+            ? {
+                name: {
+                  contains: name,
+                  mode: "insensitive",
+                },
+              }
+            : {},
+          curp
+            ? {
+                curp: {
+                  contains: curp,
+                  mode: "insensitive",
+                },
+              }
+            : {},
+        ],
+      },
+      take: limit,
+    });
+
+    return results;
+  } catch (error) {
+    console.error("Error searching employees:", error);
+
+    return [];
+  }
+};
