b8f58aafd55582e634efca0e2a765c5ddcee2f01
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-11-24 22:01:29 -0600

    feat: update config and prisma setup to include DATABASE_URL for database connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { DATABASE_URL } from "@/config";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
